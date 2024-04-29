0a57717e36038b2247962ca3a24f56d03bdc1ddd
bael4 <bayel.ryspekov@gmail.com>
2024-04-29 11:54:24 +0600

    Initial Commit

---
+++ new/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new
+//
+//  Created by Баэль Рыспеков on 29/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
