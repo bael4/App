550b2ab9afcf9e05800bba27edf744cb34ad8d71
bael4 <bayel.ryspekov@gmail.com>
2024-05-12 03:58:12 +0600

    some change

--- new/App/SceneDelegate.swift
+++ new/App/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         var window = UIWindow(windowScene: windowScene)
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window.windowScene = windowScene
-        window.rootViewController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = UINavigationController(rootViewController: TabBarController())
         window.makeKeyAndVisible()
         self.window = window
 
